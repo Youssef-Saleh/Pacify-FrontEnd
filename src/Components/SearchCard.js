56f4d1b1c20a93dac5be452fa394d38c18d58018
rokaiamohammed <rokaia.mohamed111099@gmail.com>
2020-03-31 22:08:50 +0200

    initial components of search page

---
+++ src/Components/SearchCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './SearchCard.css';
+import Pages from '../Containers/SearchPages'
+import{ Link} from'react-router-dom'
+const Card = ({CardId,IdChange,title,source,bg,Path}) => {
+    return (
+    <Link to="/Pages">  
+    <div onClick={IdChange} id={bg}  className=' truncate dib br3 pa3 ma2  bw2 shadow-5 '>
+              <h1 className="f3 lh-solid white ">{title}</h1>
+              <div className="card">
+                <img id="pod" className="db ba b--black-10 " src={source}/>
+              </div>
+          
+       </div>
+       </Link>
+      
+    );
+}
+// src={`${source}`}
+export default Card;
