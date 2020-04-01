561a641e02c9c2ff1340e48dfa5ef05ca5c99001
Ahmed Mohamed <a7amada58@gmail.com>
2020-04-01 22:20:24 +0200

    Frame fixed,integ with playlist

--- src/Containers/WebFrame.js
+++ src/Containers/WebFrame.js
@@ -4,13 +4,15 @@ import SideBar from '../Components/SideBar'
 import WebNav from '../Components/WebNav'
 import './WebFrame.css'
 import SearchPage from './SearchPage'
+import PlayList from '../Containers/Playlist'
 const WebFrame = ()=>{
     return (
         <div>
             <SideBar></SideBar>
             <WebNav></WebNav>
-            <div className='web-frame '>    {/*The Content of the page is placed here*/ }
+            <div className='web-frame '>    {/*The Content of the page is placed here,,routing also will be here*/ }
                 {/* <SearchPage></SearchPage> */}
+                <PlayList></PlayList>
             </div>
             <WebPlayer></WebPlayer>
         </div>
